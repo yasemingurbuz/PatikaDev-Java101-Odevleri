866594f94192a5583dfb8f146b0778c2473612b5
yasemingurbuz <84576138+yasemingurbuz@users.noreply.github.com>
2021-07-12 15:09:09 +0300

    Add files via upload

---
+++ Odev8.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Odev8 {
+    public static void main(String[] args) {
+        int n1, n2, select;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("İlk sayıyı giriniz : ");
+        n1 = scanner.nextInt();
+        System.out.print("İkinci sayıyı giriniz : ");
+        n2 = scanner.nextInt();
+
+        System.out.println("1-Toplama\n2-Çıkarma\n3-Çarpma\n4-Bölme");
+        System.out.print("Seçiminiz : ");
+        select = scanner.nextInt();
+        switch (select){
+            case 1 : System.out.println("Toplam : " + (n1+n2));
+                    break;
+            case 2: System.out.println("Çıkarma : " + (n1-n2));
+                    break;
+            case 3:System.out.println("Çarpma : " + (n1*n2));
+                    break;
+            case 4:
+                if(n2 != 0){System.out.println("Bölme : " + (n1/n2));}
+                else{System.out.println("Bir sayı sıfıra blünemez");
+                    break;
+                   }
+        }
+}}
