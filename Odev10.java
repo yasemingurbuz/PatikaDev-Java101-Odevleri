5c9f0e51a98e37f4d6a049be26e37d22a60e86e5
yasemingurbuz <84576138+yasemingurbuz@users.noreply.github.com>
2021-07-12 20:09:13 +0300

    Add files via upload

--- Odev10.java
+++ Odev10.java
@@ -3,31 +3,45 @@ import java.util.Scanner;
 public class Odev10 {
     public static void main(String[] args) {
         int mat, fizik, kimya, turkce, muzik;
+        int toplam=0, dersler =0;
+        double ortalama;
+
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Matematik notunuz : ");
         mat=scanner.nextInt();
+        if(0<mat && mat<100)
+        {dersler+= 1; toplam+=mat;}
+
         System.out.print("Türkçe notunuz : ");
         turkce=scanner.nextInt();
+        if(0<turkce && turkce<100)
+        {dersler+= 1; toplam+= turkce;}
+
         System.out.print("Fizik notunuz : ");
         fizik=scanner.nextInt();
+        if(0<fizik && fizik<100)
+        {dersler+= 1; toplam+=fizik;}
+
         System.out.print("Kimya notunuz : ");
         kimya=scanner.nextInt();
+        if(0<kimya && kimya<100)
+        {dersler+= 1; toplam+=kimya;}
+
         System.out.print("Müzik notunuz : ");
         muzik=scanner.nextInt();
+        if(0<muzik && muzik<100)
+        {dersler+= 1; toplam+=muzik;}
 
-        if(0<=mat && mat<=100
-        && 0<=fizik && fizik<=100
-        && 0<=kimya && kimya<=100
-        && 0<=turkce && turkce<=100
-        && 0<=muzik && muzik<=100)
-        {double avarage = (mat + turkce + fizik + kimya + muzik) / 5;
-            if(avarage <= 55){
-                System.out.println("Sınıfta kaldınız, seneye tekrar görüşmek üzere! ");
+        ortalama = toplam / dersler;
+
+        if(ortalama <= 55){
+            System.out.println("Sınıfta kaldınız, seneye tekrar görüşmek üzere! ");
             }else{
                 System.out.println("Tebrikler, sınıf geçtiniz!");
             }
-            System.out.println("Ortalamanız : " + avarage);
-        }else{System.out.println("Notları 0 ile 100 arasında girmelisiniz");}
+            System.out.println("Ortalamanız : " + ortalama);
+        }
 
         }
-}
+
