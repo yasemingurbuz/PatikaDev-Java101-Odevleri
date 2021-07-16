9e0a998be742c16736b6a32ff0d0217ee8d7bab4
yasemingurbuz <84576138+yasemingurbuz@users.noreply.github.com>
2021-07-16 14:17:56 +0300

    Add files via upload

--- Odev27.java
+++ Odev27.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Odev27 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        int n, max = 0, min = 99999, sayi;
+        int n, max = 0, min=0, sayi;
         System.out.print("Kaç Tane Sayı gireceksiniz : ");
         n = scanner.nextInt();
         for (int i = 1; i <= n; i++) {
