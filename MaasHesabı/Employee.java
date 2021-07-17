62bd4357dd65f4ff1f2395c939447c06080c35d5
yasemingurbuz <84576138+yasemingurbuz@users.noreply.github.com>
2021-07-17 18:41:45 +0300

    Add files via upload

---
+++ MaasHesabı/Employee.java
@@ -0,0 +1,62 @@
+package MaasHesabı;
+
+public class Employee {
+    String name;
+    double salary;
+    int workHours;
+    int hireYear;
+
+    public Employee(String name, double salary, int workHouse, int hireYear) {
+        this.name = name;
+        this.salary = salary;
+        this.workHours = workHouse;
+        this.hireYear = hireYear;
+    }
+    public int tax(){
+        if(salary<1000){return 0;}
+        return(int)(salary * 0.03);
+    }
+    public int bonus(){
+            return (workHours-40) *30;
+    }
+
+    int raiseSalary(){
+        int year = 2021-hireYear;
+        if( year < 10 ) return(int)( salary * 0.05 );
+        if( year > 9 && year < 20) return (int)( salary *0.10 );
+        return(int)( salary *0.15 );
+    }
+  public String toString(){
+        System.out.println("Adı : " + this.name);
+      System.out.println("Maaşı : " + this.salary);
+      System.out.println("Çalışma Saati : " + this.workHours);
+      System.out.println("Başlangıç yılı : " + this.hireYear);
+      System.out.println("Vergi : " + tax());
+      System.out.println("Bonus : " + bonus());
+      System.out.println("Maaş Artışı : " + raiseSalary());
+      System.out.println("Vergi ve Bonuslar ile birlikte maaş : " + (salary+bonus()-tax()));
+      System.out.println("Toplam Maaş : " + (salary+raiseSalary()));
+      return null;
+  }
+
+    /*
+    @Override
+    public String toString(){
+        return
+                "Adı : "+name+
+                        "\nMaaşı : "+salary+
+                        "\nÇalışma Saati : "+workHours+
+                        "\nBaşlangıç Yılı :"+hireYear+
+                        "\nVergi : "+tax()+
+                        "\nBonus : "+bonus()+
+                        "\nMaaş Artışı "+raiseSalary()+
+                        "\nVergi ve Bonuslar ile birlikte maaş : "+(salary-bonus()+tax())+
+                        "\nToplam Maaş : "+(salary+raiseSalary());
+
+    }
+*/
+}
+
+
+
+
---
+++ MaasHesabı/Main.java
@@ -0,0 +1,10 @@
+package MaasHesabı;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee employee = new Employee("yasemin",2000.0,45,1985);
+        System.out.println();
+        System.out.println(employee);
+    }
+}
+
