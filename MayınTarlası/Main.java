80cd7398c4d5e912b1edb0f4a5c0ac10a497e60a
yasemingurbuz <84576138+yasemingurbuz@users.noreply.github.com>
2021-07-20 16:28:25 +0300

    Add files via upload

---
+++ MayınTarlası/Main.java
@@ -0,0 +1,16 @@
+package MayınTarlası;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner( System.in );
+        System.out.println("Satır sayısınız giriniz : ");
+        int row = scan.nextInt();
+        System.out.println("Sütun sayısını giriniz : ");
+        int column = scan.nextInt();
+
+        MineSweeper mine = new MineSweeper(row,column);
+        mine.run();
+    }
+}
